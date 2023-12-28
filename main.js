04db572e310c6021aebbacba919132312beaaa40
Sascha Brockmann <sascha@brockmannIT.de>
2023-12-28 19:53:03 +0100

    Settings hinzugefügt, Abruf nach neuen Alarmen bei Start des Adapters, Auslagerung des Intervals

--- main.js
+++ main.js
@@ -9,6 +9,11 @@
 const utils = require("@iobroker/adapter-core");
 const axios = require("axios");
 
+// Standardwert
+const DEFAULT_MAX_POLLING = 30; // Standardwert in Sekunden
+const DEFAULT_MAX_ALARMS = 1; // Standardwert Anzahl letzte Alarme
+const DEFAULT_API_KEY = 0; // Standardwert ApiKey
+
 // Load your modules here, e.g.:
 // const fs = require("fs");
 
@@ -21,20 +26,88 @@ class Groupalarm extends utils.Adapter {
             ...options,
             name: "groupalarm",
         });
+
+        // Initialisieren Sie hier Ihre Adapter-Instanzvariablen
+        this.apiKey = 0;
+        this.maxAlarms = 0;
+        this.maxPolling = 0;
+
         this.on("ready", this.onReady.bind(this));
         this.on("stateChange", this.onStateChange.bind(this));
         // this.on("objectChange", this.onObjectChange.bind(this));
         // this.on("message", this.onMessage.bind(this));
         this.on("unload", this.onUnload.bind(this));
+
+        this.fetchAndProcessAlarms = this.fetchAndProcessAlarms.bind(this);
     }
 
-    async onReady() {
-        this.log.info("Adapter ist total fertig");
+    // API-Anfrage-Funktion
+    async fetchAndProcessAlarms() {
+        this.log.info("Fetching Alarms");
 
-        // Konfiguration lesen
-        const apiKey = this.config.apiKey; // Ihr API-Schlüssel (Personal-Access-Token)
-        this.log.info("API Key: " + apiKey);
+        try {
+            const response = await axios.get(
+                "https://app.groupalarm.com/api/v1/alarms/user",
+                {
+                    params: {
+                        limit: this.maxAlarms,
+                    },
+                    headers: {
+                        "Personal-Access-Token": this.apiKey,
+                    },
+                }
+            );
 
+            if (
+                response.data &&
+                response.data.alarms &&
+                Array.isArray(response.data.alarms)
+            ) {
+                // Erfolgreiche Antwort vom API
+                this.log.info("Alarm data fetched successfully");
+
+                // Prüfe, ob Alarme vorhanden sind
+                if (response.data.alarms.length > 0) {
+                    // Erstelle oder aktualisiere den Ordner für den aktuellsten Alarm
+                    const latestAlarm = response.data.alarms[0];
+                    const latestAlarmId = `latestalarm`;
+
+                    await this.setObjectNotExistsAsync(latestAlarmId, {
+                        type: "channel",
+                        common: {
+                            name: `Latest Alarm`,
+                        },
+                        native: {},
+                    });
+
+                    // Verarbeite den aktuellsten Alarm
+                    await this.processObject(latestAlarm, latestAlarmId);
+                }
+
+                // Pro Alarm Datenpunkt erstellen oder aktualisieren
+                for (const alarm of response.data.alarms) {
+                    const alarmBaseId = `alarms.${alarm.id}`;
+
+                    // Erstelle Haupt-Alarm-Objekt
+                    await this.setObjectNotExistsAsync(alarmBaseId, {
+                        type: "channel",
+                        common: {
+                            name: `Alarm ${alarm.id}`,
+                        },
+                        native: {},
+                    });
+
+                    // Verarbeite jedes Alarmobjekt
+                    await this.processObject(alarm, alarmBaseId);
+                }
+            }
+        } catch (error) {
+            this.log.error(`Error fetching alarm data: ${error}`);
+        }
+    }
+
+    // Rekursive Funktion, um Objekte und Arrays zu verarbeiten
+    async processObject(obj, baseId) {
         // Hilfsfunktion, um den Typ für common.type zu bestimmen
         const determineType = (value) => {
             const type = typeof value;
@@ -48,109 +121,64 @@ class Groupalarm extends utils.Adapter {
             return type;
         };
 
-        // Rekursive Funktion, um Objekte und Arrays zu verarbeiten
-        const processObject = async (obj, baseId) => {
-            for (const key in obj) {
-                const value = obj[key];
-                const stateId = `${baseId}.${key}`;
+        for (const key in obj) {
+            const value = obj[key];
+            const stateId = `${baseId}.${key}`;
 
-                if (value !== null && typeof value === "object") {
-                    // Erstelle ein Unterobjekt für Objekte und Arrays
-                    await this.setObjectNotExistsAsync(stateId, {
-                        type: Array.isArray(value) ? "channel" : "device",
-                        common: {
-                            name: key,
-                        },
-                        native: {},
-                    });
+            if (value !== null && typeof value === "object") {
+                // Erstelle ein Unterobjekt für Objekte und Arrays
+                await this.setObjectNotExistsAsync(stateId, {
+                    type: Array.isArray(value) ? "channel" : "device",
+                    common: {
+                        name: key,
+                    },
+                    native: {},
+                });
 
-                    // Rekursiver Aufruf für Unterobjekte
-                    await processObject(value, stateId);
-                } else {
-                    // Erstelle einen State für einfache Werte
-                    await this.setObjectNotExistsAsync(stateId, {
-                        type: "state",
-                        common: {
-                            name: key,
-                            type: determineType(value),
-                            role: "text",
-                            read: true,
-                            write: false,
-                        },
-                        native: {},
-                    });
+                // Rekursiver Aufruf für Unterobjekte
+                await this.processObject(value, stateId);
+            } else {
+                // Erstelle einen State für einfache Werte
+                await this.setObjectNotExistsAsync(stateId, {
+                    type: "state",
+                    common: {
+                        name: key,
+                        type: determineType(value),
+                        role: "text",
+                        read: true,
+                        write: false,
+                    },
+                    native: {},
+                });
 
-                    // Speichere den Wert im State
-                    await this.setStateAsync(stateId, {
-                        val: value,
-                        ack: true,
-                    });
-                }
+                // Speichere den Wert im State
+                await this.setStateAsync(stateId, {
+                    val: value,
+                    ack: true,
+                });
             }
-        };
+        }
+    }
+
+    async onReady() {
+        this.log.info("Adapter gestartet");
+
+        // Konfiguration lesen
+        this.apiKey = this.config.apiKey || DEFAULT_API_KEY; // Ihr API-Schlüssel (Personal-Access-Token)
+        this.maxAlarms = this.config.maxAlarms || DEFAULT_MAX_ALARMS; // Anzahl die zu holenden letzen Alarme
+        this.maxPolling = this.config.maxPolling || DEFAULT_MAX_POLLING; // Pollingzeit für die Api in Sekunden
+        this.log.info("Alarme: " + this.maxAlarms);
+        this.log.info("Zeit: " + this.maxPolling);
+        this.log.info("Api: " + this.apiKey);
+
+        // Direkter Aufruf beim Start
+        this.fetchAndProcessAlarms();
 
         // API-Anfrage-Interval
-        this.interval = setInterval(async () => {
-            try {
-                const response = await axios.get(
-                    "https://app.groupalarm.com/api/v1/alarms/user",
-                    {
-                        params: {
-                            limit: 5,
-                        },
-                        headers: {
-                            "Personal-Access-Token": apiKey,
-                        },
-                    }
-                );
-
-                if (
-                    response.data &&
-                    response.data.alarms &&
-                    Array.isArray(response.data.alarms)
-                ) {
-                    // Erfolgreiche Antwort vom API
-                    this.log.info("Alarm data fetched successfully");
-
-                    // Prüfe, ob Alarme vorhanden sind
-                    if (response.data.alarms.length > 0) {
-                        // Erstelle oder aktualisiere den Ordner für den aktuellsten Alarm
-                        const latestAlarm = response.data.alarms[0];
-                        const latestAlarmId = `latestalarm`;
-
-                        await this.setObjectNotExistsAsync(latestAlarmId, {
-                            type: "channel",
-                            common: {
-                                name: `Latest Alarm`,
-                            },
-                            native: {},
-                        });
-
-                        // Verarbeite den aktuellsten Alarm
-                        await processObject(latestAlarm, latestAlarmId);
-                    }
-
-                    // Pro Alarm Datenpunkt erstellen oder aktualisieren
-                    for (const alarm of response.data.alarms) {
-                        const alarmBaseId = `alarms.${alarm.id}`;
-
-                        // Erstelle Haupt-Alarm-Objekt
-                        await this.setObjectNotExistsAsync(alarmBaseId, {
-                            type: "channel",
-                            common: {
-                                name: `Alarm ${alarm.id}`,
-                            },
-                            native: {},
-                        });
-
-                        // Verarbeite jedes Alarmobjekt
-                        await processObject(alarm, alarmBaseId);
-                    }
-                }
-            } catch (error) {
-                this.log.error(`Error fetching alarm data: ${error}`);
-            }
-        }, 60000); // Jede Minute (60000ms)
+        this.interval = setInterval(
+            () => this.fetchAndProcessAlarms(),
+            this.maxPolling * 1000
+        );
     }
 
     /**
@@ -171,23 +199,6 @@ class Groupalarm extends utils.Adapter {
         }
     }
 
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    // 	if (obj) {
-    // 		// The object was changed
-    // 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    // 	} else {
-    // 		// The object was deleted
-    // 		this.log.info(`object ${id} deleted`);
-    // 	}
-    // }
-
     /**
      * Is called if a subscribed state changes
      * @param {string} id
@@ -204,24 +215,6 @@ class Groupalarm extends utils.Adapter {
             this.log.info(`state ${id} deleted`);
         }
     }
-
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    // 	if (typeof obj === "object" && obj.message) {
-    // 		if (obj.command === "send") {
-    // 			// e.g. send email or pushover or whatever
-    // 			this.log.info("send command");
-
-    // 			// Send response in callback if required
-    // 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-    // 		}
-    // 	}
-    // }
 }
 
 if (require.main !== module) {
